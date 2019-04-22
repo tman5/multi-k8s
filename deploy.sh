ff41ddf2edf5dcb6f5423098186c16a3aeefaeda
root <root@ip-172-31-88-159.ec2.internal>
2019-04-22 00:06:21 +0000

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t tscotto5/multi-client:latest -t tscotto5/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tscotto5/multi-server:latest -t tscotto5/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t tscotto5/multi-worker:latest -t tscotto5/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push tscotto5/multi-client:latest
+docker push tscotto5/multi-server:latest
+docker push tscotto5/multi-worker:latest
+
+docker push tscotto5/multi-client:$SHA
+docker push tscotto5/multi-server:$SHA
+docker push tscotto5/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=tscotto5/mutli-server:$SHA
+kubectl set image deployments/client-deployment client=tscotto5/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tscotto5/multi-worker:$SHA
